d35c765cb099ca1088ec340389ddb0f49e76b38e
hibana2077 <hibana2077@gmail.com>
2025-08-17 08:53:54 +0800

    fix: Increase memory allocation in GULP sweep script to 128GB

--- scripts/run_gulp_sweep.sh
+++ scripts/run_gulp_sweep.sh
@@ -3,7 +3,7 @@
 #PBS -q gpuvolta
 #PBS -l ngpus=1            
 #PBS -l ncpus=12            
-#PBS -l mem=20GB           
+#PBS -l mem=128GB           
 #PBS -l walltime=26:30:00  
 #PBS -l wd                  
 #PBS -l storage=scratch/rp06
